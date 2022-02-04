1e63b64006ae0fc56021617b39c22644c615c71b
hunucho <hunu_cho@naver.com>
2022-02-04 18:04:05 +0900

    20220204

--- d2/d2_2001.java
+++ d2/d2_2001.java
@@ -13,12 +13,12 @@ public class d2_2001 {
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
 			int n=sc.nextInt(),m=sc.nextInt(),max=0;
-			int[][]arr = new int[50][50];
+			int[][]arr = new int[n][n];
 			for(int i=0;i<n;i++)
 				for(int j=0;j<n;j++)
 					arr[i][j]=sc.nextInt();
-			for(int i=0;i<n;i++)
-				for(int j=0;j<n;j++) {
+			for(int i=0;i<=n-m;i++)
+				for(int j=0;j<=n-m;j++) {
 					int sum=0;
 					for(int k=0;k<m;k++)
 						for(int l=0;l<m;l++)
