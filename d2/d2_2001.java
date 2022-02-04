901c38b188a5e531fc8eb7537764f91d00195b7b
hunucho <hunu_cho@naver.com>
2022-02-04 17:54:51 +0900

    20220204

---
+++ d2/d2_2001.java
@@ -0,0 +1,32 @@
+package d2;
+
+import java.util.Scanner;
+
+public class d2_2001 {
+
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int n=sc.nextInt(),m=sc.nextInt(),max=0;
+			int[][]arr = new int[50][50];
+			for(int i=0;i<n;i++)
+				for(int j=0;j<n;j++)
+					arr[i][j]=sc.nextInt();
+			for(int i=0;i<n;i++)
+				for(int j=0;j<n;j++) {
+					int sum=0;
+					for(int k=0;k<m;k++)
+						for(int l=0;l<m;l++)
+							sum+=arr[i+k][j+l];
+					if(sum>max)
+						max=sum;
+				}
+			System.out.println("#"+test_case+" "+max);
+		}
+	}
+}
